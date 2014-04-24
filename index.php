d5db0bb708718556cff804a709999ef1f93e0aea
Jesse Pollak <jpollak92@gmail.com>
2014-04-24 15:44:56 -0700

    get rid of relative URL

--- index.php
+++ index.php
@@ -7,6 +7,6 @@
 <title>PHP Sample</title>
 </head>
 <body>
-<script src='https://clef.io/v2/clef.js' class='clef-button' data-app-id='1f9cacdbce6cfb103285ca7dfb8a7bde' data-redirect-url='clef.php'></script>
+<script src='https://clef.io/v3/clef.js' class='clef-button' data-app-id='1f9cacdbce6cfb103285ca7dfb8a7bde' data-redirect-url='http://localhost/clef.php'></script>
 </body>
 </html>
