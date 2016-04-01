e10fb4af8c392643ff7ad2c951d4473bc1a3ab73
Jacky Alciné <jacky@getclef.com>
2016-04-01 14:08:55 -0700

    feat(messaging): Add info about user conf/deny.

--- verify-custom-action.php
+++ verify-custom-action.php
@@ -10,7 +10,14 @@ $payload_json = json_decode($payload_str);
 
 $payload_data = $payload_json->payload_json;
 $payload_sigs = $payload_json->signatures;
+$got_sigs_for = array_keys(get_object_vars($payload_json->signatures));
 
-print "Got signatures for " . join(", ", array_keys(get_object_vars($payload_json->signatures)));
+print "Got signatures for " . join(", ", $got_sigs_for) . "<br/ >";
+
+if (in_array("user", $got_sigs_for)) {
+  print "The user confirmed!";
+} else {
+  print "The user denied.";
+}
 
 ?>
